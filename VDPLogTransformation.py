61bd394a1267099621a901eab8ae0925594bf66e
Naren <narendrans@users.noreply.github.com>
2015-11-10 14:59:51 -0500

    Update VDPLogTransformation.py

--- VDPLogTransformation.py
+++ VDPLogTransformation.py
@@ -1,5 +1,6 @@
-import sublime, sublime_plugin, re
+# Sublime plugin. Last edited by Naren 2015-11-10
 
+import sublime, sublime_plugin, re
 
 class VdpLogDateTimeTransformCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -7,8 +8,7 @@ class VdpLogDateTimeTransformCommand(sublime_plugin.TextCommand):
 		text = self.view.substr(reg)
 		self.view.replace(edit, reg, transformTimeStamp(text))
 		self.view.set_status("DateTimeTransform", "Date time transformations applied.")
-# 2015-10-19 16:10:12.194
-# 2015-10-21 14:09:25 995
+		self.view.set_syntax_file('VDPLog.tmLanguage')
 
 def transformTimeStamp(data):
 	data = re.sub(r'(2015)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d\d)', r'\1-\2-\3 \4:\5:\6.\7', data)  
